0b792dfa7332cfc81471c2c54fca1c0c95d1c761
Husein <38377122+badaevmh@users.noreply.github.com>
2022-04-08 01:06:23 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,11 +1,23 @@
 'use strict';
-document.body.addEventListener('touchstart', function () {});
+document.body.addEventListener('touchstart', function () { });
 const socialBtn = document.querySelector('.social-btn');
 const socialClose = document.querySelector('.social-close');
 const navBtn = document.querySelector('.nav-btn');
 const navClose = document.querySelector('.nav-close');
 const body = document.querySelector('body');
+const navLink = document.querySelectorAll('.nav-link');
 
+for (let i = 0; i < navLink.length; i++) {
+  navLink[i].addEventListener('mouseenter', (e) => {
+    e.preventDefault()
+    navLink[i].classList.add('glitch');
+  })
+
+  navLink[i].addEventListener('mouseleave', (e) => {
+    e.preventDefault()
+    navLink[i].classList.remove('glitch');
+  })
+}
 
 /* АНИМАЦИЯ GSAP ДЛЯ НАВИГАЦИИ И ЗАЯВКИ */
 let t1 = gsap.timeline({ paused: true });
@@ -40,54 +52,54 @@ const DroplistItem = document.querySelectorAll('.full-services__list-item')
 
 
 for (let i = 0; i < DroplistToggle.length; i++) {
-	DroplistToggle[i].addEventListener('click', (e) => {
-		e.preventDefault()
-		DroplistToggle[i].classList.toggle('active')
-		Droplist[i].classList.toggle('active')
-		DroplistArrow[i].classList.toggle('active')
-		DroplistItem[i].classList.toggle('active')
-	})
+  DroplistToggle[i].addEventListener('click', (e) => {
+    e.preventDefault()
+    DroplistToggle[i].classList.toggle('active')
+    Droplist[i].classList.toggle('active')
+    DroplistArrow[i].classList.toggle('active')
+    DroplistItem[i].classList.toggle('active')
+  })
 }
 
 
 /* СЛАЙДЕР */
 const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    slidesPerView: 3,
-    spaceBetween: 10,
-    direction: 'horizontal',
-    loop: true,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
+  // Optional parameters
+  slidesPerView: 3,
+  spaceBetween: 10,
+  direction: 'horizontal',
+  loop: true,
 
-    breakpoints: {
-      320: {
-        slidesPerView: 1,
-        spaceBetween: 2
-      },
-      // when window width is >= 480px
-      480: {
-        slidesPerView: 2,
-        spaceBetween: 5
-      },
-      // when window width is >= 640px
-      640: {
-        slidesPerView: 3,
-        spaceBetween: 10
-      }
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 2
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 2,
+      spaceBetween: 5
+    },
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 3,
+      spaceBetween: 10
     }
-  });
+  }
+});
